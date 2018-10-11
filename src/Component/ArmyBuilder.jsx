4984741fa9681320fc03e36a8918028842ec5488
Mark Dewey <mark.t.dewey@gmail.com>
2018-10-11 10:39:30 -0400

    first component!

---
+++ src/Component/ArmyBuilder.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ArmyBuilder extends Component {
+    render() {
+        return (
+            <div>
+                Here is my Army
+            </div>
+        );
+    }
+}
+
+export default ArmyBuilder;
